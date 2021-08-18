acfd5b45242872afc001c1471afee14f7885d921
dhan78 <emugundhan@gmail.com>
2021-08-18 19:06:57 -0400

    code-cleanup. moving logit into utils folder

--- oic_downloader.py
+++ oic_downloader.py
@@ -1,10 +1,5 @@
 
 from utils.pc_utils import *
-import plotly.graph_objects as go  # or plotly.express as px
-
-fig = go.Figure()  # or any Plotly Express function e.g. px.bar(...)
-# fig.add_trace( ... )
-# fig.update_layout( ... )
 
 import dash
 import dash_core_components as dcc

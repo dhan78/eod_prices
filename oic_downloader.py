09eafe9ce25d474343a7f86fe7b43792bfeefb2c
dhan78 <emugundhan@gmail.com>
2021-12-30 09:26:58 -0500

    push fema full export

--- oic_downloader.py
+++ oic_downloader.py
@@ -3,8 +3,7 @@ import functools
 from utils.pc_utils import *
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc,html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 

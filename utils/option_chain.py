991746f6e14f81c5144fed2b3124f9ef36486adc
dhan78 <emugundhan@gmail.com>
2022-06-14 21:19:41 -0400

    adding changes

--- utils/option_chain.py
+++ utils/option_chain.py
@@ -1,14 +1,26 @@
 from pc_utils import get_headers
 import pandas as pd
+import html as html_orig
 import requests
 url = 'https://api.nasdaq.com/api/quote/TSLA/option-chain?assetclass=stocks&limit=6000&fromdate=all&todate=all&excode=oprac&callput=callput&money=all&type=all'
 res = requests.get(url,headers=get_headers())
 import json
 df = pd.DataFrame(json.loads(res.text)['data']['table']['rows'])
 import numpy as np
+def convert_dt_to_str(p_dt_list):
+    return [f"{pd.to_datetime(dt).strftime('%b %d %Y')}" for dt in p_dt_list]
+
 df['expirygroup'].replace('',np.nan, inplace=True)
 df['expirygroup'].ffill(inplace=True)
+df['expirygroup']=pd.to_datetime(df.expirygroup)
+df['expirygroup']=convert_dt_to_str(df.expirygroup.values)
+
 df['drillDownURL']=df['drillDownURL'].apply(lambda x : f'https://app.quotemedia.com/quotetools/getChart?webmasterId=90423&symbol=@{x[59:] if x else x}&chscale=6m&chwid=700&chhig=300')
+df.drillDownURL = df.drillDownURL.str.replace('--','  ').values
+
+
+
+
 
 num_of_expirydts=len(sorted(df.expirygroup.unique()))
 print ('finished')
@@ -17,47 +29,37 @@ char_url = "https://app.quotemedia.com/quotetools/getChart?webmasterId=90423&sym
 def get_evenly_divided_values(value_to_be_distributed, times):
     return [value_to_be_distributed // times + int(x < value_to_be_distributed % times) for x in range(times)]
 green_range = get_evenly_divided_values(255,num_of_expirydts)
-dict_color=dict(zip(sorted(df.expirygroup.unique()),np.cumsum(green_range)))
 
+dict_color=dict(zip(df.expirygroup.unique(), np.cumsum(green_range)))
+df['color']=df.expirygroup.map(dict_color)
 
-df['expirygroup']=pd.to_datetime(df.expirygroup)
-df.strike = df.strike.astype(float)
+df[df.filter(regex='c_|p_|strike').columns] = df.filter(regex='c_|p_|strike').\
+    apply(pd.to_numeric,errors='coerce')
+df=df[df.strike>600].copy()
 
 import plotly.graph_objects as go
 
 fig=go.Figure()
-fig.update_layout( xaxis_tickfont_size=14,
-            height=600, width=1900,
-            showlegend=True,
-            hovermode='x',
-                              xaxis=dict(
-                                  tickmode='array',
-                                  tickvals=df.expirygroup.unique()),
+for expirydt, df_expiry in df.groupby('expirygroup' )[['strike','c_Last','color']]:
+    fig.add_trace(
+                    go.Scatter(x=df_expiry.strike.values, y=df_expiry.c_Last.values, name=expirydt,
+                               mode='lines', line_shape='spline', marker_color='rgb(0,300,0)', opacity=.7,
+                               line=dict(color=f'rgb(0,{dict_color.get(expirydt)},0)', width=1, )))
 
-                              )
-fig.layout.update(dict(yaxis=dict(range=[0,df.strike.max()])))
-# data=[go.Mesh3d(x=(70*np.random.randn(N)),
-#                    y=(55*np.random.randn(N)),
-#                    z=(40*np.random.randn(N)),
-#                    opacity=0.5,
-#                    color='rgba(244,22,100,0.6)'
-#                   )]
+# fig.update_layout( xaxis_tickfont_size=14,
+#             height=600, width=1900,
+#             showlegend=True,
+#             hovermode='x',
+#                               xaxis=dict(
+#                                   tickmode='array',
+#                                   # tickvals=df.strike.unique()
+#                                   ),
+#                               )
+# fig.layout.update(dict(yaxis=dict(range=[0,df.strike.max()])))
 
-df[df.filter(regex='c_|p_|strike').columns] = df.filter(regex='c_|p_|strike').apply(pd.to_numeric,
-                                                                                            errors='coerce')
-fig = go.Figure(data = [go.Mesh3d(z=df.expirygroup.values, x = df.strike.values , y=df.c_Last.values ,opacity=0.5,
-)])
 fig.update_layout( xaxis_tickfont_size=14,
             height=600, width=1900,
             showlegend=True,)
-N = 70
-
-# fig = go.Figure(data=[go.Mesh3d(x=(70*np.random.randn(N)),
-#                    y=(55*np.random.randn(N)),
-#                    z=(40*np.random.randn(N)),
-#                    opacity=0.5,
-#                    color='rgba(244,22,100,0.6)'
-#                   )])
 
 styles = {
     'pre': {
@@ -68,32 +70,37 @@ styles = {
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 from dash import Dash, dcc, html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+import dash_bootstrap_components as dbc
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='basic-interactions',
-        figure=fig
-    ),
     html.Div([
-        dcc.Markdown("""
-        **Click Data**
-
-        Click on points in the graph.
-    """),
-        html.Pre(id='click-data', style=styles['pre']),
-    ], className='three columns'),
+        dcc.Link(children="Show Option History",href='url',id='url',target='_blank'),
+        dcc.Graph(
+            id='basic-interactions',
+            figure=fig
+        )
+    ], className='eight columns'),
 
 ])
+def toggle_modal2(is_open):
+    return not is_open
 
 @app.callback(
-    Output('click-data', 'children'),
-    Input('basic-interactions', 'clickData'))
+    Output('url', 'children'),Output('url', 'href'),
+    Input('basic-interactions', 'clickData'),
+    prevent_initial_call=True
+)
 def display_click_data(clickData):
-    return json.dumps(clickData, indent=2)
+    curveNumber = clickData['points'][0]['curveNumber']
+    strike = clickData['points'][0]['x']
+    expirygroup=fig.data[curveNumber].name
+    point_mask = (df.expirygroup==expirygroup)&(df.strike==strike)
+    drillDownURL=df.loc[point_mask]['drillDownURL'].values[0]
+    return f'Show Option History {strike},[{expirygroup}]', drillDownURL
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=9900)
+    app.run_server(debug=True, host = '0.0.0.0', port=9900)
 

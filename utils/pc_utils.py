bf7ca44e37e1cdb4e9db37ba82a85833c330f7c6
dhan78 <emugundhan@gmail.com>
2022-07-11 23:40:17 -0400

    adding changes

--- utils/pc_utils.py
+++ utils/pc_utils.py
@@ -405,7 +405,7 @@ class Ticker():
             #            marker_color='rgb(300,0,0)', opacity=.4, width=.3), row=i + 1, col=1)
             fig.add_trace(
                 go.Scatter(x=df_expiry.strike.values, y=df_expiry.p_Openinterest.values/2, mode='markers',
-                           name='Call Volume_' + expirydt,
+                           name='Put Volume_' + expirydt,
                            marker=dict(size=[z/100 for z in df_expiry.p_Volume_1.fillna(0).values],
                                        color=['rgb(300,0,0)'] * len(df_expiry.c_Volume.values)), opacity=.3, ),
                 row=i + 1, col=1)

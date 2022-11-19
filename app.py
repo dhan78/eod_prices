cdbc09ad2ad03487c678bf9ca8663993108a7d7d
admin <emugundhan@gmail.com>
2022-11-19 18:44:37 -0500

    adding requirements

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash  # pip install dash
 import dash_labs as dl  # pip install dash-labs
 import dash_bootstrap_components as dbc # pip install dash-bootstrap-components
 # Code from: https://github.com/plotly/dash-labs/tree/main/docs/demos/multi_page_example1
-
+# test
 app = dash.Dash(
     __name__, use_pages=True, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.BOOTSTRAP]
 )

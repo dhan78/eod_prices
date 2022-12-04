230f84f72a1c3ba0c2a2375460e7a8cb410b854c
admin <emugundhan@gmail.com>
2022-12-03 22:30:35 -0500

    adding multi-page changes

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 import dash  # pip install dash
-import dash_labs as dl  # pip install dash-labs
-import dash_bootstrap_components as dbc # pip install dash-bootstrap-components
+# import dash_labs as dl  # pip install dash-labs
+import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
+
 # Code from: https://github.com/plotly/dash-labs/tree/main/docs/demos/multi_page_example1
 # test
 app = dash.Dash(
-    __name__, use_pages=True, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.BOOTSTRAP]
+    __name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
 navbar = dbc.NavbarSimple(
-    dbc.DropdownMenu(
-        [
-            dbc.DropdownMenuItem(page["name"], href=page["path"])
-            for page in dash.page_registry.values()
-            if page["module"] != "pages.not_found_404"
-        ],
-        nav=True,
-        label="More Pages",
-    ),
+    children=[
+        dbc.NavItem(dbc.NavLink(page["name"], href=page["path"])) for page in dash.page_registry.values()
+        if page["module"] != "pages.not_found_404"
+    ],
+    #     if page["module"] != "pages.not_found_404",
+    #     dbc.DropdownMenu(
+    #         [
+    #             dbc.DropdownMenuItem(page["name"], href=page["path"])
+    #             for page in dash.page_registry.values()
+    #             if page["module"] != "pages.not_found_404"
+    #         ],
+    #         nav=True,
+    #         label="Select Dashboard",
+    #     )
+    # ],
 
-    brand="Multi Page App Plugin Demo",
+    brand="Options Analytics",
     color="primary",
     dark=True,
     className="mb-2",
@@ -30,4 +37,4 @@ app.layout = dbc.Container(
 )
 
 if __name__ == "__main__":
-    app.run_server(debug=True,host='0.0.0.0',threaded=True)
+    app.run_server(debug=True, host='0.0.0.0', threaded=True)
---
+++ pages/option_chain.py
@@ -0,0 +1,77 @@
+import base64
+import sys
+# sys.path.insert(0, '')
+from utils.pc_utils import Nasdaq_Leap
+import dash_bootstrap_components as dbc
+import pandas as pd
+import html as html_orig
+import requests
+import json
+import plotly.graph_objects as go
+import numpy as np
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+import dash
+from dash import Dash, dcc, html, callback
+from dash.dependencies import Input, Output, State
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+dash.register_page(__name__, path='/display_option_chain',
+                   title='LEAP Options',
+                   name='LEAP Options')
+
+NL = Nasdaq_Leap()
+fig = NL.buil_leap_fig()
+
+layout = dbc.Container([
+    dbc.Row([dbc.Col([
+        html.H1(children="Option History", className='fs-3'),
+        dcc.Loading(html.Img(id='chart_id', className='shadow')),
+        html.P(children="Show Option History", id='chart_title'),
+        dcc.Interval(
+            id='interval-component',
+            interval=15 * 60 * 1000,  # 15 minute timer
+            n_intervals=0
+        )
+    ], width={'size': 9, 'offset': 3})], ),
+    dbc.Row(dbc.Col([
+        dcc.Loading(dcc.Graph(
+            id='basic-interactions',
+            figure=fig,
+            className='shadow')),
+    ], )),
+], fluid=True)
+
+
+def toggle_modal2(is_open):
+    return not is_open
+
+
+@dash.callback(
+    Output('chart_title', 'children'), Output('chart_id', 'src'),
+    Input('basic-interactions', 'clickData'),
+    prevent_initial_call=True
+)
+def display_click_data(clickData):
+    curveNumber = clickData['points'][0]['curveNumber']
+    strike = clickData['points'][0]['x']
+    expirygroup = fig.data[curveNumber].name
+    point_mask = (NL.df.expirygroup == expirygroup) & (NL.df.strike == strike)
+    drillDownURL = NL.df.loc[point_mask]['drillDownURL'].values[0]
+    encoded_image = base64.b64encode(requests.get(drillDownURL).content)
+    return f'Show Option History ${strike:,.0f} , [{expirygroup}]', 'data:image/png;base64,{}'.format(
+        encoded_image.decode())
+
+
+@dash.callback(
+    Output('basic-interactions', 'figure'),
+    Input('interval-component', 'n_intervals'),
+    prevent_initial_call=True
+)
+def refresh_nasdaq_prices(n_intervals):
+    return NL.buil_leap_fig()
+
+
+if __name__ == '__main__':
+    # pass
+    app.run_server(debug=True, host='0.0.0.0', port=9900)

42f45b4154e95f324a26bc15987ff94c36606dcf
dhan78 <emugundhan@gmail.com>
2023-06-27 11:59:30 -0400

    annotations

--- slope_lines.py
+++ slope_lines.py
@@ -25,3 +25,43 @@ fig = go.Figure(data=[slope_trace], layout=layout)
 
 # Display the chart
 fig.show()
+
+
+import plotly.graph_objects as go
+
+# Create some sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 5, 3]
+
+# Create a trace for the scatter plot
+trace = go.Scatter(x=x, y=y, mode='markers')
+
+# Create a list of annotations
+annotations = [
+    dict(
+        x=3,  # X coordinate of the annotation
+        y=1,  # Y coordinate of the annotation
+        xref='x',
+        yref='y',
+        text='Annotation Text',
+        showarrow=True,
+        arrowhead=7,
+        ax=0,
+        ay=-40
+    )
+]
+
+# Add the annotations to the trace
+trace['text'] = [''] * len(x)  # Initialize empty text to avoid overlapping labels
+for annotation in annotations:
+    trace['text'][annotation['x']-1] = annotation['text']  # -1 because Python lists are 0-indexed
+
+# Create the figure
+fig = go.Figure(data=[trace])
+
+# Update the layout to show annotations
+fig.update_layout(annotations=annotations)
+
+# Show the plot
+fig.show()
+

01867e7486ce027fdfdd3cf24c3db84dc03f8136
dhan78 <emugundhan@gmail.com>
2025-06-12 06:28:22 -0400

    Set debug mode to False for production readiness

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ app.layout = dbc.Container(
 )
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='8050' , threaded=True)
+    app.run(debug=False, host='0.0.0.0', port='8050' , threaded=True)
